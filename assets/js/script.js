3861125f2e1c212e3802b6c9c54d3440384c82bc
Knurftsucks <feyobirza2@gmail.com>
2026-03-12 16:11:03 +0100

    new page

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Your javascript goes here
+
+/* Open when someone clicks on the span element */
+function openNav(id) {
+  document.getElementById(id).style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav(id) {
+  document.getElementById(id).style.width = "0%";
+}
